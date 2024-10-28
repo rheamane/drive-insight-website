6715a6c79236a0e0060311d5092b9f922ce7ea1f
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2024-10-28 12:59:48 -0700

    Component - Benefits Section - Responsive Fixes #37

--- components/BenefitCard.tsx
+++ components/BenefitCard.tsx
@@ -3,34 +3,46 @@ const BenefitCard = ({
     description,
     icon,
     imgPath,
-    darkMode = true,
 }: {
     title: string;
     description: string;
     icon: React.ReactNode;
     imgPath: string;
-    darkMode?: boolean;
 }) => {
     return (
-        <div
-            className="flex h-[500px] flex-1 flex-col justify-between gap-10 rounded-lg bg-orange-600 p-4"
-            style={{
-                backgroundImage: `url(${imgPath})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}>
-            <div className={darkMode ? "text-slate-900" : "text-slate-200"}>
-                {icon}
-            </div>
-            <div>
-                <h1
-                    className={`font-medium ${darkMode ? "text-slate-900" : "text-slate-200"}`}>
-                    {title}
-                </h1>
-                <p
-                    className={`text-xs font-extralight ${darkMode ? "text-slate-900" : "text-slate-200"}`}>
-                    {description}
-                </p>
+        // <div
+        //     className="flex h-[500px] flex-1 flex-col justify-between gap-10 rounded-lg bg-orange-600 p-4"
+        //     style={{
+        //         backgroundImage: `url(${imgPath})`,
+        //         backgroundSize: "cover",
+        //         backgroundPosition: "center",
+        //     }}>
+        //     <div className={darkMode ? "text-slate-900" : "text-slate-200"}>
+        //         {icon}
+        //     </div>
+        //     <div>
+        //         <h1
+        //             className={`font-medium ${darkMode ? "text-slate-900" : "text-slate-200"}`}>
+        //             {title}
+        //         </h1>
+        //         <p
+        //             className={`text-xs font-extralight ${darkMode ? "text-slate-900" : "text-slate-200"}`}>
+        //             {description}
+        //         </p>
+        //     </div>
+        // </div>
+        <div>
+            <div className="flex flex-col gap-2">
+                <div className="flex w-full flex-row gap-4">
+                    {icon}
+                    <h3 className="font-semibold text-slate-200">{title}</h3>
+                </div>
+                <div>
+                    <p>{description}</p>
+                </div>
+                <div>
+                    <img src={imgPath} alt="image" />
+                </div>
             </div>
         </div>
     );
--- components/BenefitsSection.tsx
+++ components/BenefitsSection.tsx
@@ -9,20 +9,19 @@ export const BenefitsSection = () => {
                 <BenefitCard
                     title="Fuel Consumption and Cost"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                    icon={<Fuel className="h-10 w-10" />}
+                    icon={<Fuel className="h-7 w-7" />}
                     imgPath={"/benefit_fuel_2.webp"}
                 />
                 <BenefitCard
                     title="Safety and Awareness"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                    icon={<ShieldCheck className="h-10 w-10" />}
+                    icon={<ShieldCheck className="h-7 w-7" />}
                     imgPath={"/benefit_safety_1.webp"}
-                    darkMode={false}
                 />
                 <BenefitCard
                     title="Privacy Control"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                    icon={<KeyRound className="h-10 w-10" />}
+                    icon={<KeyRound className="h-7 w-7" />}
                     imgPath={"/benefit_safety_2.webp"}
                 />
             </div>
