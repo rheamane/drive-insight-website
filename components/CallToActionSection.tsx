df75cc45cff19b254f9720553360dfb7042825c2
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2025-02-13 13:53:08 -0700

    New changes and updates

--- components/CallToActionSection.tsx
+++ components/CallToActionSection.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa6";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 export const CallToActionSection = () => {
     return (
         <div>
-            <h1>Call To Action Section</h1>
-            <div className="flex flex-col items-center justify-center">
+            {/* <h1>Call To Action Section</h1> */}
+            <div className="flex flex-col items-center justify-center gap-4">
+                <Image
+                src="obd.svg"
+                height={300}
+                width={300}
+                alt="obd"
+                />
                 <Link href="/store">
                     <Button
                         className="w-full px-2 md:w-fit md:gap-4 md:pl-4"
                         size="lg">
-                        <div className="flex-1">Buy Now For $49</div>
+                        <div className="flex-1">Buy Now</div>
                         <div className="flex aspect-square items-center justify-center rounded bg-background p-1.5">
                             <FaArrowRight className="text-primary" />
                         </div>
--- components/FeaturesSection.tsx
+++ components/FeaturesSection.tsx
@@ -1,7 +1,58 @@
+import FeatureCard from "./FeatureCard";
+
+
 export const FeaturesSection = () => {
+    const features = [
+        {
+            title: "Trip Recording",
+            icon: "🚗",
+            description: "Auto start/stop for seamless trip recording.",
+        },
+        {
+            title: "Seamless OBD-II Connectivity",
+            icon: "📶",
+            description: "Fast, reliable and simple OBD-II connectivity with Bluetooth 5.0 .",
+        },
+        {
+            title: "Trip Analysis & Charts",
+            icon: "🗺️",
+            description: "View your trip route with performance charts.",
+        },
+        {
+            title: "Driving Behavior Insights",
+            icon: "📊",
+            description:
+                "Monitor braking, acceleration, speed, and distance across your trip.",
+        },
+        {
+            title: "Long-Term Trip Data Storage",
+            icon: "🗄️",
+            description:
+                "Store aggregated data forever, with a month of detailed logs.",
+        },
+        {
+            title: "Multiple Vehicle Support",
+            icon: "🚘",
+            description:
+                "Easily switch and track multiple vehicles through the app.",
+        },
+        {
+            title: "Customizable Units & Fuel Pricing",
+            icon: "⛽",
+            description: "Set fuel costs and unit preferences for expenses.",
+        },
+    ];
+
     return (
         <div>
-            <h1>Features Section</h1>
+            <div className="mx-auto mt-5 grid max-w-4xl grid-cols-1 gap-4 p-6 lg:grid-cols-3">
+            {features.slice(0, 6).map((feature, index) => (
+                <FeatureCard key={index} title={feature.title} description={feature.description} icon={feature.icon} />
+            ))}
+            <div className="col-span-1 lg:col-span-3">
+                <FeatureCard title={features[6].title} description={features[6].description} icon={features[6].icon} />
+            </div>
+        </div>
         </div>
     );
 };
